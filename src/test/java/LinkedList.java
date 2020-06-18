63ca69d01937eabdfee27f4b168bc8177ea1708f
Sharif Amiri <sharif.amiri@experian.com>
2020-06-17 23:08:57 -0700

    latest

---
+++ src/test/java/LinkedList.java
@@ -0,0 +1,15 @@
+public class LinkedList {
+
+    private Node first;
+    private Node last;
+
+    public class Node{
+        private int value;
+    }
+
+    public void addLast(int item){
+        var node = new Node();
+        node.value = item;
+
+    }
+}
--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,14 +1,35 @@
+import java.util.*;
+import java.util.LinkedList;
+
 public class Main {
 
     public static void main(String[] args) {
-        Array array = new Array(3);
-        array.insert(3);
-        array.insert(1);
-        array.insert(2);
-        array.insert(4);
-        array.removeAt(2);
-        System.out.println(array.indexOf(2));
 
-        array.print();
+        LinkedList linkedList = new LinkedList<>();
+        linkedList.add(20);
+        linkedList.add(10);
+
+        System.out.println(linkedList);
+
+        System.out.println(Arrays.toString(reorderLogFiles(new String[]{"dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"})));
     }
+
+    public static String[] reorderLogFiles(String[] logs){
+        List<String> list1 = new ArrayList<>();
+        List<String> list2 = new ArrayList<>();
+        String[] res = new String[logs.length];
+        for (int i = 0; i<logs.length; i++){
+            int index = logs[i].indexOf(" ") + 1;
+            if (logs[i].charAt(index) < 'a'){
+                list1.add(logs[i]);
+            }else {
+                list2.add(logs[i]);
+            }
+        }
+        Collections.sort(list2);
+        for (int i=0; i<list2.size(); i++) res[i] = list2.get(i);
+        for (int i=0; i<list1.size(); i++) res[list2.size()-i+1] = list1.get(i);
+        return res;
+    }
+
 }
