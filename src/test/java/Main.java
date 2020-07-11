60f0d7d34c920ea86359ee41f62aea204a83668b
Sharif Amiri <sharif.amiri@experian.com>
2020-07-10 22:49:34 -0700

    latest

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -10,6 +10,12 @@ public class Main {
         list.add(20);
         list.add(30);
 
+        Comparator<String> comparator = new Comparator<String>() {
+            @Override
+            public int compare(String s, String t1) {
+                return 0;
+            }
+        };
 
 
     }
